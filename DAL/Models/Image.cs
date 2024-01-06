5e938a1a22e84b38a0c6f3fa5d14ed0682793a3e
Matko Gečević <mgecevic@racunarstvo.hr>
2024-01-06 18:30:41 +0100

    Database scaffold and WebCoreAPI addidation

---
+++ DAL/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Models;
+
+public partial class Image
+{
+    public string Content { get; set; } = null!;
+
+    public int Id { get; set; }
+
+    public virtual ICollection<Video> Videos { get; } = new List<Video>();
+}
