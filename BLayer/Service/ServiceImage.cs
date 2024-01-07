e6ac8bfa1cd376ae3a10b2c830e293fe381c8dc8
Matko Gečević <mgecevic@racunarstvo.hr>
2024-01-07 12:44:10 +0100

    added BL

---
+++ BLayer/Service/ServiceImage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using DAL.Repos;
+
+namespace BLayer.Service
+{
+    public class ServiceImage
+    {
+        private readonly ImageRepo _repo;
+        private RwaMoviesContext _moviesContext;
+
+        public ServiceImage(RwaMoviesContext moviesContext)
+        {
+            _moviesContext = moviesContext ?? throw new ArgumentNullException(nameof(moviesContext), "MovieContext is null");
+            _repo = new ImageRepo(_moviesContext);
+        }
+
+        public IEnumerable<Image>? GetAll() => _repo.GetAll();
+
+        public Image? GetById(int id) => _repo.GetById(id);
+
+        public Image? Add(Image image)
+        {
+            if (image == null)
+            {
+                throw new InvalidOperationException("Image is null");
+            }
+            _repo.Add(image);
+            return image;
+        }
+
+        public Image? Update(Image image)
+        {
+            if (image == null)
+            {
+                throw new InvalidOperationException("Image is null");
+            }
+
+            Image? foundImage = GetById(image.Id);
+
+            if (foundImage == null)
+            {
+                throw new InvalidOperationException("Imagge not found");
+            }
+
+            _repo.Update(foundImage);
+            var updatedImage = GetById(foundImage.Id);
+            return updatedImage;
+        }
+
+
+        public void Delete(int id)
+        {
+            Image? foundImage = _repo.GetById(id);
+            if (foundImage == null)
+            {
+                throw new InvalidOperationException("Image not found");
+            }
+            _repo.Delete(foundImage.Id);
+        }
+    }
+}
---
+++ BLayer/Service/ServiceTag.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using DAL.Repos;
+
+namespace BLayer.Service
+{
+    public class ServiceTag
+    {
+        private readonly TagRepo _repo;
+        private RwaMoviesContext _moviesContext;
+
+        public ServiceTag(RwaMoviesContext moviesContext)
+        {
+            _moviesContext = moviesContext ?? throw new ArgumentNullException(nameof(moviesContext), "MovieContext is null");
+            _repo = new TagRepo(_moviesContext);
+        }
+
+        public IEnumerable<Tag>? GetAll() => _repo.GetAll();
+
+        public Tag? GetById(int id) => _repo.GetById(id);
+
+        public Tag? Add(Tag tag)
+        {
+            if (tag == null)
+            {
+                throw new InvalidOperationException("Tag is null");
+            }
+            _repo.Add(tag);
+            return tag;
+        }
+
+        public Tag? Update(Tag tag)
+        {
+            if (tag == null)
+            {
+                throw new InvalidOperationException("Tag is null");
+            }
+
+            Tag? foundTag = GetById(tag.Id);
+
+            if (foundTag == null)
+            {
+                throw new InvalidOperationException("Tag not found");
+            }
+
+            _repo.Update(foundTag);
+            var updatedTag = GetById(foundTag.Id);
+            return updatedTag;
+        }
+
+
+        public void Delete(int id)
+        {
+            Tag? foundTag = _repo.GetById(id);
+            if (foundTag == null)
+            {
+                throw new InvalidOperationException("Tag not found");
+            }
+            _repo.Delete(foundTag.Id);
+        }
+    }
+}
